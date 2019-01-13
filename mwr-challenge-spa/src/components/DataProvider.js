fc6e2a3a6b642e3f4683522b0490ec1923e8595d
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-01-13 17:37:11 +0000

    stock search working with error/limit handling, next make options selectable

--- mwr-challenge-spa/src/components/DataProvider.js
+++ mwr-challenge-spa/src/components/DataProvider.js
@@ -1,27 +1,43 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+import debounce from "../tools/debounce";
+
 class DataProvider extends Component {
     static propTypes = {
         endpoint: PropTypes.string.isRequired
     };
 
     state = {
-        searchMatches: [],
+        searchResults: null,
+        errorMessage: "",
         data: [],
         loaded: false,
         placeholder: "Loading..."
     }
 
     handleStockSelection = (event) => {
-        fetch(this.props.endpoint + "function=SYMBOL_SEARCH&keywords=" + event.target.value + "&apikey=" + process.env.REACT_APP_API_KEY)
-        .then(response => {
-            if (response.status !== 200) {
-                console.log("uh oh"); //set up placeholder/error message
-            }
-            return response.json();
-        })
-        .then(data =>  console.log(data)  /*this.setState({searchMatches: data})*/)
+        
+        event.persist()
+        debounce( //On a timer to prevent overcalling the API
+            fetch(this.props.endpoint + "function=SYMBOL_SEARCH&keywords=" + event.target.value + "&apikey=" + process.env.REACT_APP_API_KEY)
+                .then(response => {
+                    console.log(response.status)
+                    if (response.status !== 200) {
+                        this.setState({ errorMessage: "Something went wrong with the API" })
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    if (data.Note) { // The API responds with 200 instead of the appropriate response code for call limiting, so as a workaround this detects the response note
+                        this.setState({ errorMessage: "Sorry, we've hit the API call limit, try again in a minute" })
+                    } else if (data["Error Message"]) { // Again, the API still responds with 200 even if you make an invalid call
+                        // Pass    {currently the program submits a blank value when you delete what's already there, which results in the error message, might look for a more sensible approach to handle this}
+                    } else {
+                    this.setState({ searchResults: data });
+                    }
+                })
+        , 15000);
     }
 
     //turn this into a module?
@@ -36,13 +52,21 @@ class DataProvider extends Component {
         .then(data => this.setState({ data: data, loaded: true }));
     }
 
-    /*componentDidMount() {
-        this.fetchData();
-    }*/
 
     render() {
-        const { searchMatches, data, loaded, placeholder } = this.state;
-        return (
+        const { searchResults, errorMessage, data, loaded, placeholder } = this.state;
+        
+        // Assigns the search results to html elements
+        let matches
+        if (searchResults) {
+            matches = searchResults.bestMatches.map(
+                (match) => 
+                    <option key={match["1. symbol"]} value={match["1. symbol"]}> {match["2. name"]} ({match["1. symbol"]}) </option> 
+            )
+        }
+
+        // Ternary operator checks if there are any errors, if yes then presents the message to the user, if no then renders the user interface
+        return errorMessage ? <p>{errorMessage}</p> : (
             <div>
                 <form>
                     <label>
@@ -55,10 +79,11 @@ class DataProvider extends Component {
                     </label>
                 </form>
                 <p>
-                    {searchMatches.map((match) => <option key={match["1. symbol"]} value={match["1. symbol"]}>{match["2. name"]}</option>  )}
+                    {matches}
                 </p>
             </div>
         );
+        
         // If the get request was successful it returns the data to the render props, otherwise returns the placeholder (loading, or if error "something went wrong")
         //return loaded ? this.props.render(data) : <p>{placeholder}</p>;
     }
