3bc12ed344baf9852eb6c9fba9ba69d7905b6af8
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-01-13 14:18:46 +0000

    endpoint calls working

--- mwr-challenge-spa/src/components/DataProvider.js
+++ mwr-challenge-spa/src/components/DataProvider.js
@@ -7,9 +7,21 @@ class DataProvider extends Component {
     };
 
     state = {
+        searchMatches: [],
         data: [],
         loaded: false,
         placeholder: "Loading..."
+    }
+
+    handleStockSelection = (event) => {
+        fetch(this.props.endpoint + "function=SYMBOL_SEARCH&keywords=" + event.target.value + "&apikey=" + process.env.REACT_APP_API_KEY)
+        .then(response => {
+            if (response.status !== 200) {
+                console.log("uh oh"); //set up placeholder/error message
+            }
+            return response.json();
+        })
+        .then(data =>  console.log(data)  /*this.setState({searchMatches: data})*/)
     }
 
     //turn this into a module?
@@ -24,15 +36,31 @@ class DataProvider extends Component {
         .then(data => this.setState({ data: data, loaded: true }));
     }
 
-    componentDidMount() {
+    /*componentDidMount() {
         this.fetchData();
-    }
+    }*/
 
     render() {
-        const { data, loaded, placeholder } = this.state;
-
+        const { searchMatches, data, loaded, placeholder } = this.state;
+        return (
+            <div>
+                <form>
+                    <label>
+                        Which stock would you like to view? 
+                        <input 
+                            type="text" 
+                            placeholder="Microsoft" 
+                            onChange={this.handleStockSelection}
+                        />
+                    </label>
+                </form>
+                <p>
+                    {searchMatches.map((match) => <option key={match["1. symbol"]} value={match["1. symbol"]}>{match["2. name"]}</option>  )}
+                </p>
+            </div>
+        );
         // If the get request was successful it returns the data to the render props, otherwise returns the placeholder (loading, or if error "something went wrong")
-        return loaded ? this.props.render(data) : <p>{placeholder}</p>;
+        //return loaded ? this.props.render(data) : <p>{placeholder}</p>;
     }
 
 }
