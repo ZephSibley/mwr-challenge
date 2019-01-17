3a795c28ace6dda8f18975473bdf5c2c691b5209
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-01-17 12:38:38 +0000

    set up basic backend, ran first build

---
+++ mwr-challenge-spa/build/precache-manifest.49c28624fde3b845012090caf7f7e71c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a8b045422a4d9a6f0d9",
+    "url": "/static/js/main.8a8b0454.chunk.js"
+  },
+  {
+    "revision": "b798ca1ea90e35e1d2fe",
+    "url": "/static/js/1.b798ca1e.chunk.js"
+  },
+  {
+    "revision": "8a8b045422a4d9a6f0d9",
+    "url": "/static/css/main.0f3f9d58.chunk.css"
+  },
+  {
+    "revision": "ea8338dd08f08ee27d3844f37a5991fc",
+    "url": "/index.html"
+  }
+];
